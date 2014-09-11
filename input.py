48caa48186fced37918e993eafde1875f4674308
mavidser <mavidser@gmail.com>
2014-09-11 19:29:48 +0530

    python comments extraction works

--- input.py
+++ input.py
@@ -1,7 +1,31 @@
+'''input
+2
+3
+4
+5
+'''
 import sublime, sublime_plugin
+import subprocess
 import re
+import os
 
-#Use >> input redirection.
 # Extends TextCommand so that run() receives a View to modify.
-class InputCommand(sublimeplugin.TextCommand):
-  def run(self, view, args):
+class InputCommand(sublime_plugin.TextCommand):
+  def run(self,edit) :
+    result = subprocess.check_output('echo "33"',shell=True)
+    print(result.decode('utf-8'))
+    file_name = self.view.file_name()
+    filetype =  file_name[file_name.rfind('.')+1:]
+    line = self.view.substr(sublime.Region(0, self.view.size()))
+    if filetype == 'cpp' or filetype == 'c' or filetype == 'java' or filetype == 'js':
+      try:
+        problem_code = re.match('//\s*\w*\s*$', line).string.strip()[2:].strip()
+        print(problem_code)
+      except AttributeError as err:
+        print(err)
+    elif filetype == 'py' or filetype == 'pl':
+      try:     
+        problem_code = re.match("'''input\s*\n.*?\n'''",line,flags=re.S).group(0)[9:-4]
+        print(problem_code)
+      except AttributeError as err:
+        print(err)
