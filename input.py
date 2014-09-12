29134b433f60092e06a12204fbe192ab4d25bd0a
mavidser <mavidser@gmail.com>
2014-09-12 17:31:31 +0530

    python input works

--- input.py
+++ input.py
@@ -1,31 +1,38 @@
 '''input
-2
+3
 3
 4
 5
 '''
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import subprocess
 import re
-import os
+# import os
+
 
 # Extends TextCommand so that run() receives a View to modify.
 class InputCommand(sublime_plugin.TextCommand):
-  def run(self,edit) :
-    result = subprocess.check_output('echo "33"',shell=True)
-    print(result.decode('utf-8'))
+  def run(self, edit):
     file_name = self.view.file_name()
-    filetype =  file_name[file_name.rfind('.')+1:]
+
+    print(file_name)
+    filetype = file_name[file_name.rfind('.') + 1:]
+    file_name_only = file_name[:file_name.rfind('.')]
     line = self.view.substr(sublime.Region(0, self.view.size()))
-    if filetype == 'cpp' or filetype == 'c' or filetype == 'java' or filetype == 'js':
+    if filetype == 'py':
       try:
-        problem_code = re.match('//\s*\w*\s*$', line).string.strip()[2:].strip()
-        print(problem_code)
-      except AttributeError as err:
-        print(err)
-    elif filetype == 'py' or filetype == 'pl':
-      try:     
-        problem_code = re.match("'''input\s*\n.*?\n'''",line,flags=re.S).group(0)[9:-4]
-        print(problem_code)
+        user_input = re.match("'''input\s*\n.*?\n'''", line, flags=re.S).group(0)[9:-4]
+        output = self.view.window().create_output_panel('op')
+        output.run_command('erase_view')
+        try:
+          result = subprocess.check_output('echo "' + user_input + '" | python ' + file_name,
+                                           stderr=subprocess.STDOUT,
+                                           shell=True)
+          output.run_command('append', {'characters': result.decode('utf-8')})
+        except subprocess.CalledProcessError as err:
+          print(err.returncode, err.output.decode('utf-8'))
+          output.run_command('append', {'characters': err.output.decode('utf-8')})
+        self.view.window().run_command("show_panel", {"panel": "output.op"})
       except AttributeError as err:
         print(err)
