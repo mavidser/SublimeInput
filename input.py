d4faedd3ba32a114809f3d2d638f934b2a0c9472
mavidser <mavidser@gmail.com>
2014-09-13 05:31:55 +0530

    C++ works too.

--- input.py
+++ input.py
@@ -20,7 +20,31 @@ class InputCommand(sublime_plugin.TextCommand):
     filetype = file_name[file_name.rfind('.') + 1:]
     file_name_only = file_name[:file_name.rfind('.')]
     line = self.view.substr(sublime.Region(0, self.view.size()))
-    if filetype == 'py':
+    if filetype == 'cpp':
+      try:
+        user_input = re.match('/\*input\s*\n.*?\n\*/', line, flags=re.S).group(0)[8:-3]
+        output = self.view.window().create_output_panel('op')
+        output.run_command('erase_view')
+        try:
+          result = subprocess.check_output('g++ ' + file_name + ' -o ' + file_name_only,
+                                           stderr=subprocess.STDOUT,
+                                           shell=True)
+          try:
+            print('echo "' + user_input + '" |' + file_name_only)
+            result = subprocess.check_output('echo "' + user_input + '" |' + file_name_only, 
+                                           stderr=subprocess.STDOUT,
+                                           shell=True)
+            output.run_command('append', {'characters': result.decode('utf-8')})
+          except subprocess.CalledProcessError as err:
+            print(err.returncode, err.output.decode('utf-8'))
+            output.run_command('append', {'characters': err.output.decode('utf-8')})
+        except subprocess.CalledProcessError as err:
+          print(err.returncode, err.output.decode('utf-8'))
+          output.run_command('append', {'characters': err.output.decode('utf-8')})
+        self.view.window().run_command("show_panel", {"panel": "output.op"})
+      except AttributeError as err:
+        print(err)
+    elif filetype == 'py':
       try:
         user_input = re.match("'''input\s*\n.*?\n'''", line, flags=re.S).group(0)[9:-4]
         output = self.view.window().create_output_panel('op')
