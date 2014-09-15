786025fbdd3a26dadffc0a9c45b5efbafad9926f
mavidser <mavidser@gmail.com>
2014-09-15 07:31:25 +0530

    added C support

--- input.py
+++ input.py
@@ -8,10 +8,10 @@ import sublime
 import sublime_plugin
 import subprocess
 import re
+import sys
 # import os
 
 
-# Extends TextCommand so that run() receives a View to modify.
 class InputCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     file_name = self.view.file_name()
@@ -31,16 +31,40 @@ class InputCommand(sublime_plugin.TextCommand):
                                            shell=True)
           try:
             print('echo "' + user_input + '" |' + file_name_only)
-            result = subprocess.check_output('echo "' + user_input + '" |' + file_name_only, 
+            result = subprocess.check_output('echo "' + user_input + '" |' + file_name_only,
+                                             stderr=subprocess.STDOUT,
+                                             shell=True)
+            output.run_command('append', {'characters': result.decode(sys.getfilesystemencoding())})
+          except subprocess.CalledProcessError as err:
+            print(err.returncode, err.output.decode(sys.getfilesystemencoding()))
+            output.run_command('append', {'characters': err.output.decode(sys.getfilesystemencoding())})
+        except subprocess.CalledProcessError as err:
+          print(err.returncode, err.output.decode(sys.getfilesystemencoding()))
+          output.run_command('append', {'characters': err.output.decode(sys.getfilesystemencoding())})
+        self.view.window().run_command("show_panel", {"panel": "output.op"})
+      except AttributeError as err:
+        print(err)
+    elif filetype == 'c':
+      try:
+        user_input = re.match('/\*input\s*\n.*?\n\*/', line, flags=re.S).group(0)[8:-3]
+        output = self.view.window().create_output_panel('op')
+        output.run_command('erase_view')
+        try:
+          result = subprocess.check_output('gcc ' + file_name + ' -o ' + file_name_only,
                                            stderr=subprocess.STDOUT,
                                            shell=True)
-            output.run_command('append', {'characters': result.decode('utf-8')})
+          try:
+            print('echo "' + user_input + '" |' + file_name_only)
+            result = subprocess.check_output('echo "' + user_input + '" |' + file_name_only,
+                                             stderr=subprocess.STDOUT,
+                                             shell=True)
+            output.run_command('append', {'characters': result.decode(sys.getfilesystemencoding())})
           except subprocess.CalledProcessError as err:
-            print(err.returncode, err.output.decode('utf-8'))
-            output.run_command('append', {'characters': err.output.decode('utf-8')})
+            print(err.returncode, err.output.decode(sys.getfilesystemencoding()))
+            output.run_command('append', {'characters': err.output.decode(sys.getfilesystemencoding())})
         except subprocess.CalledProcessError as err:
-          print(err.returncode, err.output.decode('utf-8'))
-          output.run_command('append', {'characters': err.output.decode('utf-8')})
+          print(err.returncode, err.output.decode(sys.getfilesystemencoding()))
+          output.run_command('append', {'characters': err.output.decode(sys.getfilesystemencoding())})
         self.view.window().run_command("show_panel", {"panel": "output.op"})
       except AttributeError as err:
         print(err)
@@ -53,10 +77,10 @@ class InputCommand(sublime_plugin.TextCommand):
           result = subprocess.check_output('echo "' + user_input + '" | python ' + file_name,
                                            stderr=subprocess.STDOUT,
                                            shell=True)
-          output.run_command('append', {'characters': result.decode('utf-8')})
+          output.run_command('append', {'characters': result.decode(sys.getfilesystemencoding())})
         except subprocess.CalledProcessError as err:
-          print(err.returncode, err.output.decode('utf-8'))
-          output.run_command('append', {'characters': err.output.decode('utf-8')})
+          print(err.returncode, err.output.decode(sys.getfilesystemencoding()))
+          output.run_command('append', {'characters': err.output.decode(sys.getfilesystemencoding())})
         self.view.window().run_command("show_panel", {"panel": "output.op"})
       except AttributeError as err:
         print(err)
